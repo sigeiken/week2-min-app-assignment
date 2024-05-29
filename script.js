373f4a4877bddae8fe2aeacd28545f2bc9220531
Kenneth Kipkoech Sigei <ksigei@safaricom.co.ke>
2024-05-29 10:12:48 +0300

    added a variable to get the birthdate

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ document
   .getElementById("form")
   .addEventListener("submit", function (event) {
     event.preventDefault();
+    let dateStr = document.getElementById("birthdate").value;
     const gender = document.querySelector('input[name="gender"]:checked').value;
-
     const dayOfWeek = birthdate.getUTCDay();
     const akanNames = {
       male: ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"],
